3842091a489cc1f4346c19211c1fda1721781423
Christopher Castellano <c.castellano18@gmail.com>
2020-12-30 14:24:26 -0600

    started Big O rules

--- Big O/main.js
+++ Big O/main.js
@@ -2,15 +2,24 @@ const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
 const large = new Array(10000).fill('nemo')
 const findNemo = (array) =>{
-  let t0 = performance.now()
   for (let i = 0; i < array.length; i++){
     if(array[i] === 'nemo'){
       console.log("found Nemo")
     }
   }
-  let t1 = performance.now()
   console.log('Call took ' + (t1 - t0) + ' ms')
 }
-findNemo(nemo)
-findNemo(everyone)
-findNemo(large)
+
+findNemo(large); 
+//O(n) --> Linear Time
+//number of number of operations increases with input #.
+
+const boxes = [0,1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes){
+  console.log(boxes[0])//O(1)
+  console.log(boxes[1])//O(1)
+}
+logFirstTwoBoxes(boxes)//O(2)
+//O(1) --> Constant time 
+//runs at the same time. Predictable.
