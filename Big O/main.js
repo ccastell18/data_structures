a432b326ca36e2ac1d857d8fe9e6ded81893938c
Christopher Castellano <c.castellano18@gmail.com>
2020-12-31 18:23:56 -0600

    Big O rules

--- Big O/main.js
+++ Big O/main.js
@@ -21,5 +21,40 @@ function logFirstTwoBoxes(boxes){
   console.log(boxes[1])//O(1)
 }
 logFirstTwoBoxes(boxes)//O(2)
-//O(1) --> Constant time 
+//O(1) --> //Constant time 
 //runs at the same time. Predictable.
+
+const boxes = [1,2,3,4,5];
+//log all pairs of array
+function logAllPairsOfArray(array){
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j<array.length; j++){
+      console.log(array[i],array[j])
+    }
+  }
+}
+logAllPairsOfArray(boxes)
+//nested loops
+//O(n) * O(n) = O(n^2)
+//Quadratic time --> number of each elements multiplied by the number of operations leading to a steep curve upwards.
+//2 elements take 4 operations. 3 elements take 9 operations.
+
+function printAllNumbersThenAllPairSums(numbers){
+  console.log('these are the numbers: ')
+  numbers.forEach(function(number) {
+    console.log(number)
+  })
+
+  console.log('and these are their sums: ')
+  numbers.forEach(function(firstNumber){
+    numbers.forEach(function(secondNumber){
+      console.log(firstNumber + secondNumber)
+    })
+  })
+}
+//the first loop is O(n)
+//the second loop is O(n^2)
+//technically the function is considered O(n + n^2)
+//but we call it O(n^2) because as the input increases, n^2 is more important the n.
+//Scale is the most important part of simplification.
+
