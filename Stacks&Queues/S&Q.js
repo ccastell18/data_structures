889a54bf2aa248db7b1f79a6aaf21eca3f30e01f
Christopher Castellano <c.castellano18@gmail.com>
2021-01-12 00:16:17 -0600

    stacks and linked lists

--- Stacks&Queues/S&Q.js
+++
@@ -1,2 +0,0 @@
-// Stacks and Queues
-
---
+++ Stacks&Queues/Stack_LL.js
@@ -0,0 +1,44 @@
+// Stacks and Queues
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = next
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek(){
+    return this.top
+  }
+  push(value){
+    const newNode = new Node(value)
+    if(this.length === 0){
+      this.top = newNode;
+      this.bottom = newNode;
+    } else{
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer
+    }
+    this.length++
+    return this;
+  }
+  pop(value){
+    if(!this.top){
+      return null
+    }
+    if(this.top === this.bottom){
+      this.bottom = null
+    }
+      const holdingPointer = this.top;
+      this.top = this.top.next 
+      this.length--
+      return this
+  }
+}
