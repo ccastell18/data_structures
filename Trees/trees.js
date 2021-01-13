1fd29ccdd910cb7b434abc0c04a2fc641cce8b79
Christopher Castellano <c.castellano18@gmail.com>
2021-01-12 23:57:52 -0600

    binary function

--- Trees/trees.js
+++ Trees/trees.js
@@ -1,7 +1,27 @@
 //Binary Tree
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function BinaryTreeNode(value){
-  this.value= value;
-  this.left = null
-  this.right = null
+class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  }
+  insert(value){}
+  lookup(value){}
+}
+
+const tree = new BinarySearchTree()
+
+
+//can check work
+function traverse(node){
+  const tree = {value: node.value};
+  tree.left = node.left === null ? null : traverse(node.left)
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree
 }
