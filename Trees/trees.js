75a5e4df87bd3073e6ef18c4d3543e39804347cc
Christopher Castellano <c.castellano18@gmail.com>
2021-01-13 00:24:09 -0600

    binary tree lookup

--- Trees/trees.js
+++ Trees/trees.js
@@ -11,11 +11,53 @@ class BinarySearchTree{
   constructor(){
     this.root = null;
   }
-  insert(value){}
-  lookup(value){}
+  insert(value){
+    const newNode = new Node(value)
+    if(this.root === null){
+      this.root = newNode
+    }else{
+      let currentNode = this.root;
+      while(true){
+        if(value < currentNode.value){
+          //Left
+          if(!currentNode.left){
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left
+          }else{
+            //right
+            if(!currentNode.right){
+              currentNode.right = newNode;
+              return this;
+            }
+            currentNode = currentNode.right
+          }
+      }
+    }
+  }
+  lookup(value){
+    if(!this.root){
+      return false
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      }else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      }else if(currentNode.value === value){
+        return currentNode;
+      }
+    }
+    return false
+  }
 }
 
 const tree = new BinarySearchTree()
+tree.insert(9)
+//to check the tree path
+traverse(tree.root)
 
 
 //can check work
