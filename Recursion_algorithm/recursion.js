fc713753181264f44bb457f44674bb60ba1b196d
Christopher Castellano <c.castellano18@gmail.com>
2021-01-14 23:33:42 -0600

    fibonacci recursion

--- Recursion_algorithm/recursion.js
+++ Recursion_algorithm/recursion.js
@@ -19,3 +19,41 @@ function inception(){
 }
 inception()
 //it looks like inception(inception(inception(inception('Done")))). The done needs to bubble up so return inception within the function so each function will return the data from the next function.
+
+//Factorial Recursion
+function findFactorialRecursive(number){
+  //BaseCase
+  if(number === 2){
+    return 2
+  }//O(n)
+  //Recursive Case
+  return number * findFactorialRecursive(number - 1);
+}
+
+function findFactorialIterative(number){
+  //Base Case
+  let answer = 1;
+  if(number === 2){
+    return 2
+  }
+  //Recursive Case
+  for (let i = 2; i <= number; i++){
+    answer = answer * i
+  }
+  return answer
+}//O(n)
+
+function fibonacciIterative(n){
+  let array = [0,1]
+  for (let i = 2; i < n + 1; i++){
+    array.push(array[i-2] + array[i - 1])
+  }
+  return array[n]
+}//O(n)
+
+function fibonacciRecursive(n){
+  if(n < 2){
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+}//O(2^n)does a large binary tree. takes more calculations.
