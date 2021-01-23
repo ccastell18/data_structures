64b8ebb75e31773fcd23aa8189e002c0c86d1cb4
Christopher Castellano <c.castellano18@gmail.com>
2021-01-23 00:39:38 -0600

    dynamic programming finished

--- Dynamic_Programming/Dynamic_programming.js
+++ Dynamic_Programming/Dynamic_programming.js
@@ -33,3 +33,20 @@ function fibonacci(n){
   }
   return fibonacci(n-1) + fibonacci(n-2)
 }
+
+//Fibonnaci O(n)
+function fibonacci(){
+  let cache = {}
+  return function fib(n){
+    if(n in cache){
+      return cache[n]
+    }else{
+      if(n < 2){
+        return n;
+      } else{
+        cache[n]=fib(n-1) + fib(n-2)
+        return cache[n];
+      }
+    }
+    }
+  }
